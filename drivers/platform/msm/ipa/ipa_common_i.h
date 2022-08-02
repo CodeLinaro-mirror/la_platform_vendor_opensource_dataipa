fdf17711051617150ae793018b4dc570a89ca844
quic_ysaispur <quic_ysaispur@quicinc.com>
2022-08-02 00:55:00 -0700

    Revert "msm: ipa: EasyMesh support for R1"
    This reverts commit ba9b9b7026245191f5e547e7b2c1d1c9c7ac54d6.
    
    Change-Id: I275654240114525f8215b710e2e5468cb5b3b9b2

--- drivers/platform/msm/ipa/ipa_common_i.h
+++ drivers/platform/msm/ipa/ipa_common_i.h
@@ -604,8 +604,7 @@ void ipa3_ntn_uc_dereg_rdyCB(void);
 
 int ipa3_conn_wdi3_pipes(struct ipa_wdi_conn_in_params *in,
 	struct ipa_wdi_conn_out_params *out,
-	ipa_wdi_meter_notifier_cb wdi_notify,
-	bool ast_update);
+	ipa_wdi_meter_notifier_cb wdi_notify);
 
 int ipa3_disconn_wdi3_pipes(int ipa_ep_idx_tx, int ipa_ep_idx_rx,
 	int ipa_ep_idx_tx1);
