3cd4526f009cb6c682b35bfcb0b50ea1aa80f3f9
Fakruddin Vohra <quic_fakruddi@quicinc.com>
2022-12-15 18:45:06 +0530

    msm: ipa3: Add support for wikiki chip
    Addded change to enable support for wikiki chipset
    
    Change-Id: I84623f77f8f9e091359684c27c583f69280c66b4
    Signed-off-by: Fakruddin Vohra <quic_fakruddi@quicinc.com>

--- drivers/platform/msm/gsi/gsi.h
+++ drivers/platform/msm/gsi/gsi.h
@@ -166,6 +166,7 @@ enum gsi_evt_chtype {
 	GSI_EVT_CHTYPE_NTN_EV = 0xD,
 	GSI_EVT_CHTYPE_WDI3M_EV = 0xE,
 	GSI_EVT_CHTYPE_WDI3_V2_EV = 0XF,
+	GSI_EVT_CHTYPE_WDI4_EV = 0X10,
 };
 
 enum gsi_evt_ring_elem_size {
@@ -257,6 +258,7 @@ enum gsi_chan_prot {
 	GSI_CHAN_PROT_NTN = 0xD,
 	GSI_CHAN_PROT_WDI3M = 0xE,
 	GSI_CHAN_PROT_WDI3_V2 = 0XF,
+	GSI_CHAN_PROT_WDI4 = 0X10,
 };
 
 enum gsi_max_prefetch {
@@ -948,6 +950,40 @@ struct __packed gsi_11ad_tx_channel_scratch {
 	uint32_t fixed_data_buffer_size_pow_2:16;
 	uint32_t resv2:8;
 };
+
+/**
+ * gsi_wdi4_wikiki_channel_scratch - WDI 4 protocol, wikiki chipset
+ * SW config area of channel scratch
+ *
+ * @wifi_rx_rp_addr_low: Low 32 bits of Transfer ring Read Index address.
+ * @wifi_rx_rp_addr_high: High 32 bits of Transer ring Read Index address.
+ * @update_rp_moderation_threshold: Threshold N for Transfer ring Read Index
+           N is the number of packets that IPA will
+           process before wifi transfer ring Ri will
+           be updated.
+ * @endp_metadata_reg_offset: Rx only, the offset of IPA_ENDP_INIT_HDR_METADATA_n
+            of the corresponding endpoint in 4B words from IPA
+            base address.
+ * @qmap_id: Rx only, used for setting metadata register in IPA, Read only field
+            for MCS, Write for SW
+ * @bank_id: TX only, used for setting BANK ID in event ring element
+ * @vdev_id: TX only, used for setting VDEV ID in event ring element
+ * @pmac_id: TX only, used for setting PMAC ID in event ring element
+ */
+
+struct __packed gsi_wdi4_channel_scratch {
+	uint32_t wifi_rp_address_low;
+	uint32_t wifi_rp_address_high;
+	uint32_t update_rp_moderation_threshold : 5;
+	uint32_t qmap_id : 8;
+	uint32_t reserved1 : 3;
+	uint32_t endp_metadata_reg_offset : 16;
+	uint32_t rx_pkt_offset : 16;
+	uint32_t bank_id : 6;
+	uint32_t vdev_id : 8;
+	uint32_t pmac_id : 2;
+};
+
 /**
  * gsi_wdi3_hamilton_channel_scratch - WDI 3 protocol, hamilton chipset
  * SW config area of channel scratch
@@ -1149,6 +1185,7 @@ union __packed gsi_channel_scratch {
 	struct __packed gsi_11ad_tx_channel_scratch tx_11ad;
 	struct __packed gsi_wdi3_channel_scratch wdi3;
 	struct __packed gsi_wdi3_v2_channel_scratch wdi3_v2;
+	struct __packed gsi_wdi4_channel_scratch wdi4;
 	struct __packed gsi_mhip_channel_scratch mhip;
 	struct __packed gsi_wdi2_channel_scratch_new wdi2_new;
 	struct __packed gsi_aqc_channel_scratch aqc;
