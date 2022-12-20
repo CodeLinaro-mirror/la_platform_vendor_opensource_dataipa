1452c0e4c81d15f59af701e9a94d78544f89a958
Chaitanya Pratapa <quic_cpratapa@quicinc.com>
2022-12-20 13:47:58 -0800

    dataipa: align to the new API signatures
    Make changes to use the new API signatures per the latest
    kernel upgrade.
    
    Change-Id: I57f8a17ce1dfd2070a3341c1aba809f68ed0b0c6
    Signed-off-by: Chaitanya Pratapa <quic_cpratapa@quicinc.com>
    (cherry picked from commit e419512cfa0aa5237047bb15d71820ad81c9ccc4)
    (cherry picked from commit cf3ef0309ffde8e0b1cd8951750e1292d43047c5)

--- drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
@@ -1636,9 +1636,16 @@ int ipa_setup_sys_pipe(struct ipa_sys_connect_params *sys_in, u32 *clnt_hdl)
 	if (IPA_CLIENT_IS_PROD(sys_in->client) &&
 		ipa3_ctx->tx_napi_enable) {
 		if (sys_in->client == IPA_CLIENT_APPS_LAN_PROD) {
+#if (LINUX_VERSION_CODE > KERNEL_VERSION(6, 0, 14))
+			netif_napi_add_tx_weight(&ipa3_ctx->generic_ndev,
+			&ep->sys->napi_tx, tx_completion_func,
+			NAPI_TX_WEIGHT);
+#else
 			netif_tx_napi_add(&ipa3_ctx->generic_ndev,
 			&ep->sys->napi_tx, tx_completion_func,
 			NAPI_TX_WEIGHT);
+
+#endif
 			ep->sys->napi_tx_enable = ipa3_ctx->tx_napi_enable;
 			ep->sys->tx_poll = ipa3_ctx->tx_poll;
 		} else if(sys_in->client == IPA_CLIENT_APPS_WAN_PROD ||
@@ -1646,9 +1653,15 @@ int ipa_setup_sys_pipe(struct ipa_sys_connect_params *sys_in, u32 *clnt_hdl)
 			sys_in->client == IPA_CLIENT_APPS_WAN_LOW_LAT_DATA_PROD||
 			sys_in->client == IPA_CLIENT_IPSEC_ENCAP_PROD ||
 			sys_in->client == IPA_CLIENT_IPSEC_DECAP_PROD) {
+#if (LINUX_VERSION_CODE > KERNEL_VERSION(6, 0, 14))
+			netif_napi_add_tx_weight((struct net_device *)sys_in->priv,
+			&ep->sys->napi_tx, tx_completion_func,
+			NAPI_TX_WEIGHT);
+#else
 			netif_tx_napi_add((struct net_device *)sys_in->priv,
 			&ep->sys->napi_tx, tx_completion_func,
 			NAPI_TX_WEIGHT);
+#endif
 			ep->sys->napi_tx_enable = ipa3_ctx->tx_napi_enable;
 			ep->sys->tx_poll = ipa3_ctx->tx_poll;
 		} else {
@@ -1664,8 +1677,13 @@ int ipa_setup_sys_pipe(struct ipa_sys_connect_params *sys_in, u32 *clnt_hdl)
 	}
 
 	if (sys_in->client == IPA_CLIENT_APPS_WAN_LOW_LAT_DATA_CONS) {
+#if (LINUX_VERSION_CODE > KERNEL_VERSION(6, 0, 14))
+		netif_napi_add((struct net_device *)sys_in->priv,
+			&ep->sys->napi_rx, ipa3_rmnet_ll_rx_poll);
+#else
 		netif_napi_add((struct net_device *)sys_in->priv,
 			&ep->sys->napi_rx, ipa3_rmnet_ll_rx_poll, NAPI_WEIGHT);
+#endif
 		napi_enable(&ep->sys->napi_rx);
 	}
 
