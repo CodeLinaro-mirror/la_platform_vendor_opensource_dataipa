cf46923a1a0db5b81e676e08f7888b0ebe810389
Pooja Kumari <quic_kumarip@quicinc.com>
2023-01-23 14:26:58 +0530

    msm: ipa4: enable IPA power collapse after uC is loaded
    Devote IPA clocks after IPA uC is loaded.
    
    Change-Id: I27d33ad5dd948b104c9e8aa033cb507d8312aba3

--- drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/rmnet_ipa.c
@@ -4401,8 +4401,12 @@ static int ipa3_lcl_mdm_ssr_notifier_cb(struct notifier_block *this,
 			ipa3_qmi_service_exit();
 			ipa3_q6_pre_powerup_cleanup();
 		}
-		/* hold a proxy vote for the modem. */
-		ipa3_proxy_clk_vote(atomic_read(&rmnet_ipa3_ctx->is_ssr));
+		/* hold a proxy vote for the modem.
+		 * for IPA 4.0 offline charge is not needed and proxy vote
+		 * is already held.
+		 */
+		if (ipa3_ctx->ipa_hw_type != IPA_HW_v4_0)
+			ipa3_proxy_clk_vote(atomic_read(&rmnet_ipa3_ctx->is_ssr));
 		ipa3_reset_freeze_vote();
 		IPAWANINFO("IPA BEFORE_POWERUP handling is complete\n");
 		break;
