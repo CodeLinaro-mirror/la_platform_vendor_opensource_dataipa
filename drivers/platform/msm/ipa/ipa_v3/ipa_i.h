7a5861771464cd137a74b57b220069957cfd1a4b
Chaitanya Pratapa <quic_cpratapa@quicinc.com>
2023-02-07 16:28:08 -0800

    msm: ipa: map ethernet registers to AP CB
    Make changes to map ethernet registers to AP CB
    for GSI to access.
    
    Change-Id: I9179d4df6a777c352a4d07dbefbdb5bd02d2624c
    Signed-off-by: Chaitanya Pratapa <quic_cpratapa@quicinc.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa_i.h
+++ drivers/platform/msm/ipa/ipa_v3/ipa_i.h
@@ -2501,6 +2501,7 @@ struct ipa3_context {
 	bool gsi_ch20_wa;
 	bool s1_bypass_arr[IPA_SMMU_CB_MAX];
 	u32 wdi_map_cnt;
+	u32 eth_map_cnt;
 	struct wakeup_source *w_lock;
 	struct ipa3_wakelock_ref_cnt wakelock_ref_cnt;
 	/* RMNET_IOCTL_INGRESS_FORMAT_AGG_DATA */
