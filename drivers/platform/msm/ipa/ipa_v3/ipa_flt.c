0c72b5f9b22792087fd065a07499734ea1e0ad9d
Michael Adisumarta <quic_madisuma@quicinc.com>
2023-06-14 16:32:44 -0700

    dataipa: ep flt config check for V2X
    Added condition to check flt configuration needs to be skipped or not
    
    Change-Id: I6481fab5ad7696825987cbe22bdbe80e79df7aa0
    Signed-off-by: Michael Adisumarta <quic_madisuma@quicinc.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa_flt.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_flt.c
@@ -461,6 +461,13 @@ static bool ipa_flt_skip_pipe_config(int pipe)
 		return true;
 	}
 
+	if ((ipa3_get_ep_mapping(IPA_CLIENT_APPS_WAN_V2X_PROD) == pipe
+		&& ipa3_ctx->modem_cfg_emb_pipe_flt)
+		&& ep->client == IPA_CLIENT_APPS_WAN_V2X_PROD) {
+		IPADBG_LOW("skip %d\n", pipe);
+		return true;
+	}
+
 	if ((ipa3_get_ep_mapping(IPA_CLIENT_APPS_WAN_ETH_PROD) == pipe
 		&& ipa3_ctx->modem_cfg_emb_pipe_flt)
 		&& ep->client == IPA_CLIENT_APPS_WAN_ETH_PROD) {
