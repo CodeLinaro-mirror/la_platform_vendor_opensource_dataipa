6c6e52e31b76f13e3c4fb9bb188346f28ec2af73
Raghavendar rao l <quic_rlomte@quicinc.com>
2023-09-25 17:19:36 +0530

    msm: ipa3: change to handle the wlan callback after uc loading
    Added change to handle the wlan callbacks for all the wlan
    instance, previously their was support to call only one callback.
    
    Change-Id: I3d3e368d25869a70c65bab21cd2a438b4739e67b
    Signed-off-by: Raghavendar rao l <quic_rlomte@quicinc.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa_i.h
+++ drivers/platform/msm/ipa/ipa_v3/ipa_i.h
@@ -1895,6 +1895,8 @@ struct ipa3_uc_wdi_ctx {
 #ifdef IPA_WAN_MSG_IPv6_ADDR_GW_LEN
 	ipa_wdi_meter_notifier_cb stats_notify;
 #endif
+	struct list_head ready_cb_list;
+	struct mutex lock;
 };
 
 /**
