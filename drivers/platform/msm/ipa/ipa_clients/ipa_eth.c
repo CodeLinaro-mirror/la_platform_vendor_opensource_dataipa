0fae6ae657a966d0ef17c5b7d36fad88949da9a4
Veerendra Kumar Kamsali <quic_veekum@quicinc.com>
2023-12-04 20:05:42 +0530

    msm: ipa3:ipa_eth_get_config_type_internal is  not returning from failure case
    ipa_eth_get_config_type_internal is not returning from failure case.
    
    Change-Id: I5f5e6900cef7ae807c0aab4e96d4f408c75b6ee2

--- drivers/platform/msm/ipa/ipa_clients/ipa_eth.c
+++ drivers/platform/msm/ipa/ipa_clients/ipa_eth.c
@@ -1486,6 +1486,7 @@ static int ipa_eth_get_config_type_internal(
 	if (!eth_config) {
 		IPA_ETH_ERR("Invalid parameter\n");
 		ret = -EFAULT;
+		return ret;
 	}
 
 	IPA_ETH_DBG("Config type requested for client %d, inst_id %d\n", client_type, inst_id);
