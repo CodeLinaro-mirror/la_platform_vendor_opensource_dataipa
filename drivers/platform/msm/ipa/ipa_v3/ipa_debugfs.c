eddf2f1ed3338edc79cc98101f9b3865d1689744
Ilia Lin <quic_ilial@quicinc.com>
2024-01-30 15:03:44 +0200

    ipa: Disable IPsec by default and enable via ipa_config
    The IPsec feture will be disbaled by default, and only enabled via
    ipa_config.txt. Endpoints will be configured to 3 round processing,
    only if the IPsec is enabled.
    In order to enable the IPsec, write "ipsec" to the ipa_config.txt
    and reboot.
    
    Change-Id: I08102061bdf0928192ab1f7944c55dbe9f51e179
    Signed-off-by: Ilia Lin <quic_ilial@quicinc.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa_debugfs.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_debugfs.c
@@ -428,6 +428,7 @@ int _ipa_read_ep_reg_v4_0(char *buf, int max_len, int pipe)
 		"IPA_ENDP_INIT_HOL_TIMER_%u=0x%x\n"
 		"IPA_ENDP_INIT_DEAGGR_%u=0x%x\n"
 		"IPA_ENDP_INIT_CFG_%u=0x%x\n"
+		"IPA_ENDP_INIT_SEQ_%u=0x%x\n"
 		"IPA_ENDP_INIT_IPSEC_CFG_%u=0x%x\n"
 		"IPA_ENDP_INIT_DRBIP_CFG_%u=0x%x\n",
 		pipe, ipahal_read_reg_n(IPA_ENDP_INIT_NAT_n, pipe),
@@ -441,6 +442,7 @@ int _ipa_read_ep_reg_v4_0(char *buf, int max_len, int pipe)
 		pipe, ipahal_read_reg_n(IPA_ENDP_INIT_HOL_BLOCK_TIMER_n, pipe),
 		pipe, ipahal_read_reg_n(IPA_ENDP_INIT_DEAGGR_n, pipe),
 		pipe, ipahal_read_reg_n(IPA_ENDP_INIT_CFG_n, pipe),
+		pipe, ipahal_read_reg_n(IPA_ENDP_INIT_SEQ_n, pipe),
 		pipe, ipahal_read_reg_n(IPA_ENDP_INIT_IPSEC_CFG_n, pipe),
 		pipe, ipahal_read_reg_n(IPA_ENDP_INIT_DRBIP_CFG_n, pipe));
 }
