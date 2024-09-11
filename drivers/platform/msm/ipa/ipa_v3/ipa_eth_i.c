5df0253189e4e74a81898b4d90fea5269e04e3ac
Abhishek Raghuvanshi <quic_araghuva@quicinc.com>
2024-09-11 14:24:15 -0700

    dataipa: Update holb timer values.
    Updated holb timer values for qos and best effort pipes.
    
    Change-Id: I35de23d074bf16ca8cfb84f7831b9661f55ebcd1
    Signed-off-by: Abhishek Raghuvanshi <quic_araghuva@quicinc.com>
    (cherry picked from commit dc5a28c425e899ab27172fcd8afc4c6f5bd4b0d3)

--- drivers/platform/msm/ipa/ipa_v3/ipa_eth_i.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_eth_i.c
@@ -69,7 +69,7 @@ enum ipa_eth_dir {
 };
 
 /* HOLB timeout values for QOS. */
-u32 qos_holb_tmr[IPA_ETH_MAX_TX_DMA_CHANNEL_QOS] = {2000, 500};
+u32 qos_holb_tmr[IPA_ETH_MAX_TX_DMA_CHANNEL_QOS] = {31000, 500};
 
 static void ipa_iemac_smmu_cb_save_mapping_i(enum ipa_smmu_cb_type cb_type, phys_addr_t pa,
 	unsigned long iova, size_t len, int instance_id, enum ipa_eth_pipe_direction dir, u8 pipe_idx)
