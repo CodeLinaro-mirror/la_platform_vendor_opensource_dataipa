13007ca04313ad66b0bcee5cd71547bb5468e9ce
Raghavendar rao l <quic_rlomte@quicinc.com>
2024-11-05 11:41:09 +0530

    msm: ipa3: update to empty threshold value
    Change to update the empty threshold register value for wlan->ipa pipes.
    To handle the throughput requirements for HMT chipsets.
    
    Signed-off-by: Raghavendar rao l <quic_rlomte@quicinc.org>
    Change-Id: Ief5fdb268ed5ad825bc0ee50255977f241261319

--- drivers/platform/msm/ipa/ipa_v3/ipa_wdi3_i.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_wdi3_i.c
@@ -280,10 +280,20 @@ static int ipa3_setup_wdi3_gsi_channel(u8 is_smmu_enabled,
 
 	gsi_channel_props.use_db_eng = GSI_CHAN_DB_MODE;
 	gsi_channel_props.max_prefetch = GSI_ONE_PREFETCH_SEG;
+
+	/* Updated threshold value specifically for HMT to
+	   handle the throughput. */
+	if ((gsi_channel_props.dir == GSI_CHAN_DIR_TO_GSI) &&
+				(ipa_get_wdi_version() == IPA_WDI_3_V2)) {
+		gsi_channel_props.empty_lvl_threshold = 15;
+	}
+	else {
+		gsi_channel_props.empty_lvl_threshold =
+			gsi_ep_info->prefetch_threshold;
+	}
+
 	gsi_channel_props.prefetch_mode =
 		gsi_ep_info->prefetch_mode;
-	gsi_channel_props.empty_lvl_threshold =
-		gsi_ep_info->prefetch_threshold;
 	gsi_channel_props.low_weight = 1;
 	gsi_channel_props.err_cb = ipa3_wdi3_gsi_chan_err_cb;
 
