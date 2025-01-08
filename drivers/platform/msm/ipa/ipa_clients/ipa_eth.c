75381d3fcc3973cf251117740ca439bcaea40796
Anshul Kundra <quic_akundra@quicinc.com>
2025-01-09 01:20:52 +0530

    dataipa: Allocate valid memory for eth cb
    This fix allocate valid memory to eth callback
    for late IPA ready scenario
    
    Change-Id: I45a51244df80761df365a9b5cae4a6932556b1e2
    (cherry picked from commit aeca380b65b5344be4b8c1af38c6ba96e96a971a)

--- drivers/platform/msm/ipa/ipa_clients/ipa_eth.c
+++ drivers/platform/msm/ipa/ipa_clients/ipa_eth.c
@@ -306,6 +306,7 @@ static void ipa_eth_ready_notify_work(struct work_struct *work)
 			entry->info->notify(entry->info->userdata);
 		/* remove from list once notify is done */
 		list_del(&entry->link);
+		kfree(entry->info); // free cb
 		kfree(entry);
 	}
 	mutex_unlock(&ipa_eth_ctx->lock);
@@ -314,13 +315,17 @@ static void ipa_eth_ready_notify_work(struct work_struct *work)
 static int ipa_eth_register_ready_cb_internal(struct ipa_eth_ready *ready_info)
 {
 	int rc;
-	struct ipa_eth_ready_cb_wrapper *ready_cb;
+	struct ipa_eth_ready_cb_wrapper *ready_cb = NULL;
+	struct ipa_eth_ready *cb = NULL;
 
 	/* validate user input */
 	if (!ready_info) {
 		IPA_ETH_ERR("null ready_info");
 		return -EFAULT;
 	}
+
+	IPA_ETH_DBG("DBG: ETH CB :%lx cb:%lx udata: %lx\n",ready_info,
+			ready_info->notify, ready_info->userdata);
 
 	if (!ipa_eth_ctx) {
 		rc = ipa_eth_init_internal();
@@ -331,14 +336,23 @@ static int ipa_eth_register_ready_cb_internal(struct ipa_eth_ready *ready_info)
 		}
 		IPA_ETH_DBG("ipa_eth register ready cb\n");
 		mutex_lock(&ipa_eth_ctx->lock);
-		ready_cb = kmalloc(sizeof(struct ipa_eth_ready_cb_wrapper),
+		ready_cb = kzalloc(sizeof(struct ipa_eth_ready_cb_wrapper),
 			GFP_KERNEL);
 		if (!ready_cb) {
 			mutex_unlock(&ipa_eth_ctx->lock);
 			ipa_eth_cleanup_internal();
 			return -ENOMEM;
 		}
-		ready_cb->info = ready_info;
+		cb = kzalloc(sizeof(struct ipa_eth_ready), GFP_KERNEL);
+		if(NULL == cb) {
+			IPA_ETH_DBG("Error in alloc cb struct\n");
+			return -ENOMEM;
+		}
+		cb->notify = ready_info->notify;
+		cb->userdata = ready_info->userdata;
+
+		ready_cb->info = cb;
+
 		list_add_tail(&ready_cb->link, &ipa_eth_ctx->ready_cb_list);
 		mutex_unlock(&ipa_eth_ctx->lock);
 		/* rely on uC ready callback, only register once */
@@ -351,13 +365,22 @@ static int ipa_eth_register_ready_cb_internal(struct ipa_eth_ready *ready_info)
 		/* assume only IOSS could register for cb */
 		IPA_ETH_ERR("multiple eth register happens\n");
 		mutex_lock(&ipa_eth_ctx->lock);
-		ready_cb = kmalloc(sizeof(struct ipa_eth_ready_cb_wrapper),
+		ready_cb = kzalloc(sizeof(struct ipa_eth_ready_cb_wrapper),
 			GFP_KERNEL);
+
 		if (!ready_cb) {
 			mutex_unlock(&ipa_eth_ctx->lock);
 			return -ENOMEM;
 		}
-		ready_cb->info = ready_info;
+		cb = kzalloc(sizeof(struct ipa_eth_ready), GFP_KERNEL);
+		if(NULL == cb) {
+			IPA_ETH_DBG("Error in alloc cb struct\n");
+			return -ENOMEM;
+		}
+		cb->notify = ready_info->notify;
+		cb->userdata = ready_info->userdata;
+
+		ready_cb->info = cb;
 		list_add_tail(&ready_cb->link, &ipa_eth_ctx->ready_cb_list);
 		/* if already ready, directly callback from wq */
 		if (ipa3_uc_loaded_check())
@@ -1423,7 +1446,7 @@ static int ipa_eth_client_reg_intf_internal(struct ipa_eth_intf_info *intf)
 
 	/* populate tx prop */
 	if (tx.num_props) {
-		tx_prop = kmalloc(
+		tx_prop = kzalloc(
 			sizeof(*tx_prop) * tx.num_props *
 			IPA_IP_MAX, GFP_KERNEL);
 		if (!tx_prop) {
@@ -1467,7 +1490,7 @@ static int ipa_eth_client_reg_intf_internal(struct ipa_eth_intf_info *intf)
 	}
 	/* populate rx prop */
 	if (rx.num_props) {
-		rx_prop = kmalloc(
+		rx_prop = kzalloc(
 			sizeof(*rx_prop) * rx.num_props *
 			IPA_IP_MAX, GFP_KERNEL);
 		if (!rx_prop) {
