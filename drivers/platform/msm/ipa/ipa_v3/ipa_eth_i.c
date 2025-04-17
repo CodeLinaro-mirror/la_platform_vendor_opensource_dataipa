ec92ee738f181866cb2ae711cf3e7b2fb38a0b04
Ashok Vuyyuru <quic_avuyyuru@quicinc.com>
2025-04-17 19:25:56 +0530

    dataipa: add mapping smmu mapping for PCIE1 RTK ETH attach
    In PCIE1 BAR address HW not accessabile directly by IPA HW,
    so creating SMMU mapping to access PCIE1 BAR address.
    
    
    Change-Id: Id0e9ce20b1fcf370e3581aef810583edc73ebd3c
    (cherry picked from commit ef3a92aa7c8db582bd372d57394484690661e6ab)
    (cherry picked from commit c2649dc6fe1eea2278b6f153a4f67b910bb1d80b)

--- drivers/platform/msm/ipa/ipa_v3/ipa_eth_i.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_eth_i.c
@@ -451,7 +451,7 @@ static void ipa_eth_gsi_chan_err_cb(struct gsi_chan_err_notify *notify)
 
 static int ipa_eth_setup_rtk_gsi_channel(
 	struct ipa_eth_client_pipe_info *pipe,
-	struct ipa3_ep_context *ep)
+	struct ipa3_ep_context *ep, u8 pipe_idx)
 {
 	struct gsi_evt_ring_props gsi_evt_ring_props;
 	struct gsi_chan_props gsi_channel_props;
@@ -461,6 +461,7 @@ static int ipa_eth_setup_rtk_gsi_channel(
 	int result, len;
 	int queue_number;
 	u64 bar_addr;
+	unsigned long iova;
 
 	if (unlikely(!pipe->info.is_transfer_ring_valid)) {
 		IPAERR("RTK transfer ring invalid\n");
@@ -468,9 +469,16 @@ static int ipa_eth_setup_rtk_gsi_channel(
 		return -EFAULT;
 	}
 
-	/* setup event ring */
-	bar_addr =
-		IPA_ETH_PCIE_SET(pipe->info.client_info.rtk.bar_addr);
+	if(pipe->client_info->client_type == IPA_ETH_CLIENT_RTK8111K) {
+		/* setup event ring */
+		result = ipa_iemac_smmu_cb_add_mapping_pa(IPA_SMMU_CB_AP,
+			pipe->info.client_info.rtk.bar_addr, 8, true, &iova, pipe->client_info->inst_id, pipe->dir, pipe_idx);
+		bar_addr = iova;
+	} else {
+		/* setup event ring */
+		bar_addr =
+			IPA_ETH_PCIE_SET(pipe->info.client_info.rtk.bar_addr);
+	}
 	memset(&gsi_evt_ring_props, 0, sizeof(gsi_evt_ring_props));
 	/*Setting up EV for RTK8111K*/
 	if(pipe->client_info->client_type == IPA_ETH_CLIENT_RTK8111K)
@@ -1395,7 +1403,7 @@ int ipa3_eth_connect(
 	switch (prot) {
 	case IPA_HW_PROTOCOL_RTK:
 	case IPA_HW_PROTOCOL_RTK3:
-		result = ipa_eth_setup_rtk_gsi_channel(pipe, ep);
+		result = ipa_eth_setup_rtk_gsi_channel(pipe, ep, pipe_idx);
 		break;
 	case IPA_HW_PROTOCOL_AQC:
 		result = ipa_eth_setup_aqc_gsi_channel(pipe, ep);
