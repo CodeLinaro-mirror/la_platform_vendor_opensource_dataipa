1ea4dbcc710eb3e662ffaaf454fd7d3c689978e5
Pavan Kumar M <rpavan@qti.qualcomm.com>
2025-04-25 15:15:10 +0530

    msm:ipa: Disable replication on command prod pipe
    Control packet information is not needed in DPL logging.
    Disable replication on command producer pipe to avoid
    control packets in DPL logs. Update the control pipe
    to non-dma mode & update the metadata with dest_ep info
    while sending the data.
    
    Set pkt_init_dst_ep_remote when targeting the Q6
    WAN consumer endpoint. This ensures correct remote
    endpoint handling for packet initialization in IPA
    metadata, aligning with expected behavior for Q6 WAN
    routing.
    
    Change-Id: I682c45d9840282247f5e9ecbc8685f7e5b1c686f
    Signed-off-by: Pavan Kumar M <rpavan@qti.qualcomm.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa_utils.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_utils.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022, 2024-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <net/ip.h>
@@ -5685,7 +5685,7 @@ static const struct ipa_ep_configuration ipa3_ep_mapping
 	[IPA_5_5][IPA_CLIENT_APPS_WAN_LOW_LAT_PROD] = {
 			true, IPA_v5_5_GROUP_URLLC,
 			false,
-			IPA_DPS_HPS_SEQ_TYPE_DMA_ONLY,
+			IPA_DPS_HPS_SEQ_TYPE_2ND_PKT_PROCESS_PASS_NO_DEC_UCP,
 			QMB_MASTER_SELECT_DDR,
 			{ 4, 9, 8, 16, IPA_EE_AP, GSI_SMART_PRE_FETCH, 3	},
 			IPA_TX_INSTANCE_NA },
