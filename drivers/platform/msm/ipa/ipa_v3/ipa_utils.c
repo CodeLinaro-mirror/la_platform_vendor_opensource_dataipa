1ff33a527364b551acbe71b00e1f0bb70ea7a963
Pavan Kumar M <quic_rpavan@quicinc.com>
2025-06-10 12:02:47 +0530

    msm: ipa: Modify EP configuration for ODL_DPL_CONS
    ODL_DPL_CONS & USB_DPL_CONS have same EP configuration resulting
    in crash. If gsi channel USB pipe is in allocated state & AP tries
    to suspend AP pipes, gsi channel for ODL pipe will be stopped.
    
    Since the same chanel is shared between the two pipes & channel is
    in allocated state, assert will be triggered.
    
    Change-Id: I10318eeb4fca493b0051aae10ce4e326f91ccef9
    Signed-off-by: Pavan Kumar M <quic_rpavan@quicinc.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa_utils.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_utils.c
@@ -5602,7 +5602,7 @@ static const struct ipa_ep_configuration ipa3_ep_mapping
 			false,
 			IPA_DPS_HPS_SEQ_TYPE_INVALID,
 			QMB_MASTER_SELECT_DDR,
-			{ 19, 2, 5, 5, IPA_EE_AP, GSI_ESCAPE_BUF_ONLY, 0}, IPA_TX_INSTANCE_UL },
+			{ 18, 17, 9, 9, IPA_EE_AP, GSI_ESCAPE_BUF_ONLY, 0}, IPA_TX_INSTANCE_UL },
 
 	[IPA_5_2_IOT][IPA_CLIENT_ETHERNET_CONS] = {
 			true, IPA_v5_2_GROUP_DL,
