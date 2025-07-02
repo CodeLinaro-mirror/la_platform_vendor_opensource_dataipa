762c48cff85802c8260fd934a1024905eac11160
Sheshivardhan <sparupat@qti.qualcomm.com>
2025-07-02 09:23:30 +0530

    dataipa: Added extra checks when uc cb is triggered
    Check if eth ctx is allocated or not in uc ready
    callback to avoid NULL access
    
    Change-Id: I0efcf79c50c5e5f2c5e26a87251caffd7cb9dcf4
    (cherry picked from commit d67626c448535540011b610b38e0cba8dd718d6f)
    Signed-off-by: Sushant Phalke<sphalke@qti.qualcomm.com>

--- drivers/platform/msm/ipa/ipa_clients/ipa_eth.c
+++ drivers/platform/msm/ipa/ipa_clients/ipa_eth.c
@@ -295,7 +295,10 @@ static int ipa_eth_uc_rdy_cb(struct notifier_block *nb,
 	unsigned long action, void *data)
 {
 	IPA_ETH_DBG("IPA uC is ready for eth");
-	queue_work(ipa_eth_ctx->wq, &ipa_eth_ready_notify);
+	if (!ipa_eth_ctx)
+		IPA_ETH_ERR("ipa_eth_ctx isn't initialized\n");
+	else
+		queue_work(ipa_eth_ctx->wq, &ipa_eth_ready_notify);
 	return NOTIFY_OK;
 }
 
