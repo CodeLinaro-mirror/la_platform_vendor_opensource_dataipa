814bcb72d3a218955947adbe4ed06afa52bf8291
Omri Cohen <omric@qti.qualcomm.com>
2025-11-20 03:22:50 -0800

    ipa: Add support to SDXECHO and SA535M configurations in Kbuild files Add config files for IPAv7.0
    Change-Id: I70819342543d122aae10a9360888f9edba7343d5
    Signed-off-by: Omri Cohen <omric@qti.qualcomm.com>

---
+++ config/dataipa_hw7_0.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+* Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.​
+*/
+
+#define CONFIG_GSI 1
+#define CONFIG_RMNET_IPA3 1
+#define CONFIG_RNDIS_IPA 1
+#define CONFIG_IPA_WDI_UNIFIED_API 1
+#define CONFIG_ECM_IPA 1
+#define CONFIG_IPA3 1
+#define CONFIG_IPA3_REGDUMP 1
+#define CONFIG_IPA3_REGDUMP_IPA_7_0 1
+
