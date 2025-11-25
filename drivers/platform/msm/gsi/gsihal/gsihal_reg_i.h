CodeLinaro-mirror/la_platform_vendor_opensource_dataipa

commit 358dc57bae03f16b10824e6dad6e18133cae4899

dataipa: IPA7.0 driver upgrade
Signed-off-by: Ilia Lin <ilial@qti.qualcomm.com>
Signed-off-by: Eliad Ben Yishay <quic_ebenyish@quicinc.com>
Change-Id: I234439e7029aa54a16790e2f83989f5cfcb1975b

drivers/platform/msm/gsi/gsihal/gsihal_reg_i.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
 * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+* Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.?
 */
 
 #ifndef _GSIHAL_REG_I_H_
@@ -13,7 +14,11 @@
 		(((reg) & (mask)) >> (shift))
 
 /* GSI_GSI_INST_RAM_n */
+#define GSI_INST_RAM_n_MAXn(iram_size, num_psis) (((iram_size) * 2) + ((num_psis) * 2) -1)
+
 #define GSI_GSI_INST_RAM_n_WORD_SZ 0x4
+#define GSI_V7_0_IRAM_SIZE (8 * 1024)
+#define GSI_V7_0_NUM_PSIS 48
 
 /* GSI_GSI_SHRAM_n */
 #define GSI_GSI_SHRAM_n_WORD_SZ 0x4
@@ -27,6 +32,7 @@
 #define GSI_V3_0_GSI_INST_RAM_n_MAXn 8255
 #define GSI_V5_2_GSI_INST_RAM_n_MAXn 6207
 #define GSI_V6_0_GSI_INST_RAM_n_MAXn 10303
+#define GSI_V7_0_GSI_INST_RAM_n_MAXn GSI_INST_RAM_n_MAXn(GSI_V7_0_IRAM_SIZE, GSI_V7_0_NUM_PSIS)
 
 /* GSI_EE_n_CNTXT_TYPE_IRQ */
 #define GSI_EE_n_CNTXT_TYPE_IRQ_GENERAL_BMSK 0x40
@@ -123,6 +129,15 @@
 #define GSI_EE_n_CNTXT_GSI_IRQ_STTS_GSI_BREAK_POINT_BMSK 0x1
 #define GSI_EE_n_CNTXT_GSI_IRQ_STTS_GSI_BREAK_POINT_SHFT 0x0
 
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_STTS_GSI_MCS_STACK_OVRFLOW_BMSK 0xff000000
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_STTS_GSI_MCS_STACK_OVRFLOW_SHFT 0x18
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_STTS_GSI_CMD_FIFO_OVRFLOW_BMSK 0x10000
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_STTS_GSI_CMD_FIFO_OVRFLOW_SHFT 0x10
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_STTS_GSI_BUS_ERROR_BMSK 0x100
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_STTS_GSI_BUS_ERROR_SHFT 0x8
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_STTS_GSI_BREAK_POINT_BMSK 0xff
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_STTS_GSI_BREAK_POINT_SHFT 0x0
+
 /* GSI_EE_n_CNTXT_TYPE_IRQ */
 #define GSI_EE_n_CNTXT_TYPE_IRQ_GENERAL_BMSK 0x40
 #define GSI_EE_n_CNTXT_TYPE_IRQ_GENERAL_SHFT 0x6
@@ -199,8 +214,8 @@
 #define GSI_V3_0_EE_n_GSI_HW_PARAM_2_GSI_NUM_CH_PER_EE_SHFT 0x0
 
 /* GSI_EE_n_GSI_HW_PARAM_4 */
-#define GSI_V3_0_EE_n_GSI_HW_PARAM_4_GSI_IRAM_PROTCOL_CNT_BMSK 0xff00
-#define GSI_V3_0_EE_n_GSI_HW_PARAM_4_GSI_IRAM_PROTCOL_CNT_SHFT 0x8
+#define GSI_V3_0_EE_n_GSI_HW_PARAM_4_GSI_IRAM_PROTOCOL_CNT_BMSK 0xff00
+#define GSI_V3_0_EE_n_GSI_HW_PARAM_4_GSI_IRAM_PROTOCOL_CNT_SHFT 0x8
 #define GSI_V3_0_EE_n_GSI_HW_PARAM_4_GSI_NUM_EV_PER_EE_BMSK 0xff
 #define GSI_V3_0_EE_n_GSI_HW_PARAM_4_GSI_NUM_EV_PER_EE_SHFT 0x0
 
@@ -348,5 +363,57 @@
 #define GSI_EE_n_CNTXT_GSI_IRQ_EN_GSI_BUS_ERROR_SHFT 0x1
 #define GSI_EE_n_CNTXT_GSI_IRQ_EN_GSI_BREAK_POINT_BMSK 0x1
 #define GSI_EE_n_CNTXT_GSI_IRQ_EN_GSI_BREAK_POINT_SHFT 0x0
+
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_EN_GSI_MCS_STACK_OVRFLOW_BMSK 0xff000000
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_EN_GSI_MCS_STACK_OVRFLOW_SHFT 0x18
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_EN_GSI_CMD_FIFO_OVRFLOW_BMSK 0x10000
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_EN_GSI_CMD_FIFO_OVRFLOW_SHFT 0x10
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_EN_GSI_BUS_ERROR_BMSK 0x100
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_EN_GSI_BUS_ERROR_SHFT 0x8
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_EN_GSI_BREAK_POINT_BMSK 0xff
+#define GSI_V7_0_EE_n_CNTXT_GSI_IRQ_EN_GSI_BREAK_POINT_SHFT 0x0
+
+/**
+ * GSI_IRQ_2_MCS_MAPPING_ACCn_TABLE - valid for table0 and
+ * table1 registers. table0 and table1 registers have the same
+ * bitmasks and shift in ipa7.0.
+ */
+#define GSI_IRQ_2_MCS_MAPPING_ACCn_TABLE_VALID_MCS_FOR_IRQ3_BMSK 0xff000000
+#define GSI_IRQ_2_MCS_MAPPING_ACCn_TABLE_VALID_MCS_FOR_IRQ3_SHFT 0x18
+#define GSI_IRQ_2_MCS_MAPPING_ACCn_TABLE_VALID_MCS_FOR_IRQ2_BMSK 0xff0000
+#define GSI_IRQ_2_MCS_MAPPING_ACCn_TABLE_VALID_MCS_FOR_IRQ2_SHFT 0x10
+#define GSI_IRQ_2_MCS_MAPPING_ACCn_TABLE_VALID_MCS_FOR_IRQ1_BMSK 0xff00
+#define GSI_IRQ_2_MCS_MAPPING_ACCn_TABLE_VALID_MCS_FOR_IRQ1_SHFT 0x8
+#define GSI_IRQ_2_MCS_MAPPING_ACCn_TABLE_VALID_MCS_FOR_IRQ0_BMSK 0xff
+#define GSI_IRQ_2_MCS_MAPPING_ACCn_TABLE_VALID_MCS_FOR_IRQ0_SHFT 0x0
+
+/**
+ * GSI_MAX_TRE_TLV_n - an array of 4-bits elements, organized in
+ * an array of registers, represent the max number of
+ * TLVs that can be processed per NEW_RE IRQ for each channel.
+ * Each register is 32 bit long, GSI_MAX_TRE_TLV_0[0:3] are for
+ * channel id 0, GSI_MAX_TRE_TLV_0[28:31] are for channel id 7.
+ * GSI_MAX_TRE_TLV_n[i:i+3] are for channel id
+ * ((8 * n) + (i / 4)). GSI_MAX_TRE_TLV_n_MAXn is 9 for gsi7.0
+ * meaning there are 7 such registers.
+ */
+#define GSI_MAX_TRE_TLV_n_MAXn 9
+#define GSI_MAX_TRE_TLV_n_BITS_PER_ELEMENT 4
+#define GSI_MAX_TRE_TLV_n_BMSK 0xffffffff
+#define GSI_MAX_TRE_TLV_n_SHFT 0x0
+
+/**
+ * GSI_DEBUG_MUTEX_REGION_TYPE_01
+ */
+#define GSI_DEBUG_MUTEX_REGION_TYPE_01_REGION_TYPE_1_BMSK 0xffff0000
+#define GSI_DEBUG_MUTEX_REGION_TYPE_01_REGION_TYPE_1_SHFT 0x10
+
+/**
+ * GSI_DEBUG_MUTEX_REGION_TYPE_23
+ */
+#define GSI_DEBUG_MUTEX_REGION_TYPE_23_REGION_TYPE_3_BMSK 0xffff0000
+#define GSI_DEBUG_MUTEX_REGION_TYPE_23_REGION_TYPE_3_SHFT 0x10
+#define GSI_DEBUG_MUTEX_REGION_TYPE_23_REGION_TYPE_2_BMSK 0xffff
+#define GSI_DEBUG_MUTEX_REGION_TYPE_23_REGION_TYPE_2_SHFT 0
 
 #endif /* _GSIHAL_REG_I_H_ */

drivers/platform/msm/include/linux/msm_gsi.h
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2015-2020, The Linux Foundation. All rights reserved.
  * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.?
  */
 
 #ifndef MSM_GSI_H
@@ -11,7 +12,8 @@
 
 enum gsi_chan_dir {
 	GSI_CHAN_DIR_FROM_GSI = 0x0,
-	GSI_CHAN_DIR_TO_GSI = 0x1
+	GSI_CHAN_DIR_TO_GSI = 0x1,
+	GSI_CHAN_DIR_MAX
 };
 
 /**

drivers/platform/msm/include/uapi/linux/msm_ipa.h
@@ -598,10 +598,32 @@ enum ipa_client_type {
 	IPA_CLIENT_ETHERNET_CONS3		= 153,
 
 	IPA_CLIENT_ETHERNET_PROD4		= 154,
-	IPA_CLIENT_ETHERNET_CONS4		= 155
+	IPA_CLIENT_ETHERNET_CONS4		= 155,
+
+	IPA_CLIENT_WLAN1_QOS_PROD		= 156,
+
+	IPA_CLIENT_WLAN2_QOS_PROD		= 158,
+	IPA_CLIENT_WLAN2_QOS_CONS		= 159,
+
+	IPA_CLIENT_WLAN3_QOS_PROD		= 160,
+	IPA_CLIENT_WLAN2_QOS_CONS1		= 160,
+
+	IPA_CLIENT_WLAN4_QOS_PROD		= 162,
+
+	IPA_CLIENT_WLAN4_PROD			= 164,
+
+	IPA_CLIENT_ETHERNET_QOS_PROD		= 166,
+
+	IPA_CLIENT_ETHERNET2_QOS_PROD		= 168,
+
+	IPA_CLIENT_ETHERNET3_QOS_PROD		= 170,
+
+	IPA_CLIENT_CRYPTO_OFFLOAD_PROD		= 172,
+
+	IPA_CLIENT_PLACEHOLDER
 };
 
-#define IPA_CLIENT_MAX (IPA_CLIENT_ETHERNET_CONS4 + 1)
+#define IPA_CLIENT_MAX (IPA_CLIENT_PLACEHOLDER + 1)
 
 #define IPA_CLIENT_WLAN2_PROD IPA_CLIENT_A5_WLAN_AMPDU_PROD
 #define IPA_CLIENT_Q6_DL_NLO_DATA_PROD IPA_CLIENT_Q6_DL_NLO_DATA_PROD
@@ -859,7 +881,19 @@ enum ipa_flt_action {
 	IPA_PASS_TO_ROUTING,
 	IPA_PASS_TO_SRC_NAT,
 	IPA_PASS_TO_DST_NAT,
-	IPA_PASS_TO_EXCEPTION
+	IPA_PASS_TO_EXCEPTION,
+	IPA_PASS_TO_OUT_IPV4_CT_NAT,
+	IPA_PASS_TO_IN_IPV4_CT_NAT,
+	IPA_FLT_ACTION_MAX
+};
+
+enum ipa_fltrt_rule_type {
+	IPA_FLT_RULE_TYPE_IP,
+	IPA_FLT_RULE_TYPE_ETH,
+	IPA_FLT_RULE_TYPE_ETH_IP,
+	IPA_FLT_RULE_TYPE_ETH_NON_IP,
+	IPA_FLT_RULE_TYPE_NON_IP,
+	IPA_FLT_RULE_TYPE_MAX
 };
 
 /**
@@ -1209,9 +1243,10 @@ enum ipa_hw_type {
 	IPA_HW_v5_2 = 23,
 	IPA_HW_v5_5 = 24,
 	IPA_HW_v6_0 = 25,
+	IPA_HW_v7_0 = 26,
 };
 
-#define IPA_HW_MAX (IPA_HW_v6_0 + 1)
+#define IPA_HW_MAX (IPA_HW_v7_0 + 1)
 
 #define IPA_HW_v4_0 IPA_HW_v4_0
 #define IPA_HW_v4_1 IPA_HW_v4_1
@@ -1225,6 +1260,7 @@ enum ipa_hw_type {
 #define IPA_HW_v5_2 IPA_HW_v5_2
 #define IPA_HW_v5_5 IPA_HW_v5_5
 #define IPA_HW_v6_0 IPA_HW_v6_0
+#define IPA_HW_v7_0 IPA_HW_v7_0
 
 /**
  * enum ipa_hw_feature_support - IPA HW supported feature
@@ -1443,6 +1479,8 @@ struct ipa_ipfltri_rule_eq {
 	 * values: IS-FRAG-0, Is-Primary-1, Is-Secondary-2, Not-Frag-3
 	 */
 	uint8_t is_frag_encoding;
+	uint8_t version;
+	uint32_t rule_eq_bitmap_v3;
 };
 
 /**
@@ -1546,6 +1584,8 @@ struct ipa_flt_rule_v2 {
 	uint8_t ttl_update;
 	uint8_t qos_class;
 	uint8_t esp_after_udp;
+	uint16_t cnt_idx_v2;
+	enum ipa_fltrt_rule_type rule_type;
 };
 
 /**

commit d47b95b19b1d8687c852ef7bcefae34657d0056b

ipa: Code review fixes for the IPAv7.0 driver upgrade
Fixes: 358dc57bae03f16b10824e6dad6e18133cae4899

Change-Id: I61a7e4f961348a9439f5a5b33fd3b3104aa508d4
Signed-off-by: Ilia Lin <ilial@qti.qualcomm.com>

drivers/platform/msm/gsi/gsihal/gsihal_reg_i.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
 * Copyright (c) 2020, The Linux Foundation. All rights reserved.
-* Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.?
+* Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.​
 */
 
 #ifndef _GSIHAL_REG_I_H_
@@ -17,7 +17,7 @@
 #define GSI_INST_RAM_n_MAXn(iram_size, num_psis) (((iram_size) * 2) + ((num_psis) * 2) -1)
 
 #define GSI_GSI_INST_RAM_n_WORD_SZ 0x4
-#define GSI_V7_0_IRAM_SIZE (8 * 1024)
+#define GSI_V7_0_IRAM_SIZE (15 * 512)
 #define GSI_V7_0_NUM_PSIS 48
 
 /* GSI_GSI_SHRAM_n */

drivers/platform/msm/include/linux/msm_gsi.h
@@ -2,7 +2,7 @@
 /*
  * Copyright (c) 2015-2020, The Linux Foundation. All rights reserved.
  * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.?
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #ifndef MSM_GSI_H

drivers/platform/msm/include/uapi/linux/msm_ipa.h
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
  * Copyright (c) 2021-2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.​
  */
 
 #ifndef _UAPI_MSM_IPA_H_
@@ -910,8 +911,6 @@ enum ipa_flt_action {
 	IPA_PASS_TO_SRC_NAT,
 	IPA_PASS_TO_DST_NAT,
 	IPA_PASS_TO_EXCEPTION,
-	IPA_PASS_TO_OUT_IPV4_CT_NAT,
-	IPA_PASS_TO_IN_IPV4_CT_NAT,
 	IPA_FLT_ACTION_MAX
 };
 
@@ -1591,6 +1590,8 @@ struct ipa_flt_rule {
  * @ttl_update: bool to indicate whether TTL update is needed or not.
  * @qos_class: QOS classification value.
  * @esp_after_udp: bool for ESP after UDP (NAT-T) rules.
+ * @rule_type: IP/ETH/NonIP etc.
+ * @cnt_idx_v2: Stats cnt index for IPAv7 and on
  */
 struct ipa_flt_rule_v2 {
 	uint8_t retain_hdr;

drivers/platform/msm/ipa/ipa_test_module/ipa_test_module_impl.c
@@ -2,6 +2,7 @@
 /*
 * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
 * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.​
 */
 
 #include <linux/types.h>	/* u32 */

drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
@@ -3,6 +3,7 @@
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
  * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.​
  */
 #include <linux/ip.h>
 #include <linux/ipv6.h>

drivers/platform/msm/ipa/ipa_v3/ipa_dt_replacement.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.​
  */
 
 #include <linux/errno.h>

drivers/platform/msm/ipa/ipa_v3/ipa_flt.c
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
  * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.​
  */
 
 #include "ipa_i.h"

drivers/platform/msm/ipa/ipa_v3/ipa_rt.c
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
  * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.​
  */
 
 #include <linux/bitops.h>

drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal.c
@@ -1317,8 +1317,10 @@ static struct ipahal_imm_cmd_pyld *ipa_imm_cmd_construct_ip_v6_ct_init(
 		(struct ipahal_imm_cmd_ip_v6_ct_init *)params;
 
 	pyld = IPAHAL_MEM_ALLOC(sizeof(*pyld) + sizeof(*data), is_atomic_ctx);
-	if (unlikely(!pyld))
+	if (unlikely(!pyld)) {
+		IPAHAL_ERR("kzalloc err\n");
 		return pyld;
+	}
 	pyld->opcode = ipahal_imm_cmd_get_opcode(cmd);
 	pyld->len = sizeof(*data);
 	data = (struct ipa_imm_cmd_hw_ip_v6_ct_init *)pyld->data;
@@ -1347,8 +1349,10 @@ static struct ipahal_imm_cmd_pyld *ipa_imm_cmd_construct_ip_v6_ct_init_v7_0(
 		(struct ipahal_imm_cmd_ip_v6_ct_init *)params;
 
 	pyld = IPAHAL_MEM_ALLOC(sizeof(*pyld) + sizeof(*data), is_atomic_ctx);
-	if (unlikely(!pyld))
+	if (unlikely(!pyld)) {
+		IPAHAL_ERR("kzalloc err\n");
 		return pyld;
+	}
 	pyld->opcode = ipahal_imm_cmd_get_opcode(cmd);
 	pyld->len = sizeof(*data);
 	data = (struct ipa_imm_cmd_hw_ip_v6_ct_init_v7_0 *)pyld->data;
@@ -2363,7 +2367,7 @@ static void ipa_parse_gen_pkt_v7_0(struct ipahal_pkt_status *status, const void 
 	status->flt_tbl_idx = hw_status->ipa_pkt.flt_table_idx;
 	status->frag_rule = hw_status->ipa_pkt.frag_rule;
 	status->metadata_origin = hw_status->ipa_pkt.metadata_origin;
-	
+
 	status->flt_miss = (hw_status->ipa_pkt.flt_rule_id == IPAHAL_PKT_STATUS_FLTRT_RULE_MISS_ID);
 	status->rt_miss = (hw_status->ipa_pkt.rt_rule_id == IPAHAL_PKT_STATUS_FLTRT_RULE_MISS_ID);
 

drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal.h
@@ -234,6 +234,25 @@ struct ipahal_imm_cmd_table_dma {
 	u8 base_addr;
 };
 
+/*
+ * struct ipahal_imm_cmd_table_dma_v7_0 - TABLE_DMA cmd payload
+ * Perform DMA operation on NAT and IPV6 connection tracking related mem
+ * addresses. Copy data into different locations within IPv6CT and NAT
+ * associated tbls. (For add/remove NAT rules)
+ * @table_index: NAT tbl index. Defines the tbl on which to perform DMA op.
+ * @table_Select: Base addr to which the DMA operation should be performed:
+ *   00: NAT/v4 connection tracking Table
+ *   01: NAT/v4 connection tracking Index Table
+ *   10: IPv6 connection tracking Table
+ * @offset_within_entry: Byte offset within entry
+ * @entry_index: NAT/Connection tracking Entry index
+ * @data: data to be written
+ * @cache_entry_evict: Evict corresponding NAT/CT entry info in the cache
+ * @no_write: Skip Write operation, allowing cache entry eviction without update
+ * @cache_entry_hash_value: Hash value identifying the NAT/CT cache info for eviction
+ * @write_bitmask: Specifies which bits will be written, for example 0x03
+ *   means that only two least significant bits will be updated
+ */
 struct ipahal_imm_cmd_table_dma_v7_0 {
     u8 table_index;
     u8 table_Select;
@@ -266,9 +285,9 @@ struct ipahal_imm_cmd_ip_packet_init {
  * @hdr_removal_insertion_disable: true - disabled, false - enabled
  * @cs_disable: true - disabled, false - enabled
  * @quota_tethering_stats_disable: true - disabled, false - enabled
- * fields @flt_rt_tbl_idx - @flt_retain_hdr are a logical software translation
+ * fields @flt_rt_tbl_idx - @flt_retain_hdr, @flt_rule_type are a logical software translation
  * of ipa5_0_flt_rule_hw_hdr/ipa5_5_flt_rule_hw_hdr
- * fields @rt_pipe_dest_idx - @rt_system are a logical software translation
+ * fields @rt_pipe_dest_idx - @rt_system, @rt_hpc_fetch_len are a logical software translation
  * ipa5_0_rt_rule_hw_hdr/ipa5_5_flt_rule_hw_hdr
  * @dpl_disable: true - disabled, false - enabled, valid from IPAv5_5.
  * @flt_ext_hdr: true - flt ext_hdr enabled, false - disabled. Note all fields of
@@ -279,6 +298,18 @@ struct ipahal_imm_cmd_ip_packet_init {
  * ext header are valid in immediate command irrespective of this flag.
  * fields @rt_ttl - @rt_esp_after_udp are a logical software translation
  * ipa6_0_rt_rule_hw_hdr_ext
+ * @conn_track_nat_stats_direction: CT/NAT "All Packets" Stats direction (0=SrcNAT, 1=DstNAT)
+ * @conn_track_nat_stats_ip_type: CT/NAT "All Packets" Stats IP type: (0=IPv4, 1=IPv6)
+ * @traffic_mode: Traffic mode:
+ *   0: Non-DMA IP mode
+ *   1: Non-DMA Ethernet mode
+ *   2: Non-DMA Unstructured mode
+ *   3: DMA
+ * @leading_header_size: size of header before the packet/message,
+ *   the offset to the traffic mode content (e.g., size of L2 header or L2 without Ethernet)
+ * @conn_track_nat_stats_counter_idx: Set the packet's CT/NAT "All Packets" Stats Counter index.
+ *   0 means not valid.
+ * @sw_classification_cookie: Default Producer Classification SW Cookie value
  */
 struct ipahal_imm_cmd_ip_packet_init_ex {
 	bool frag_disable;
@@ -449,6 +480,19 @@ struct ipahal_imm_cmd_dma_task_32b_addr {
 	u32 packet_size;
 };
 
+/*
+ * struct ipahal_imm_cmd_shaping_control - Immediate cmd shaping control
+ * @traffic_class_bitmap: Each bit indicates the corresponding traffic class index,
+ *   starting with traffic class 1.
+ * @shaped_prod_token_bucket_reinit_bitmap: Each bit indicates the corresponding
+ *   shaped producer index whose token bucket will be reinitialized.
+ * @traffic_class_operation: Operation is one of the following:
+ *   0: Unhalt + reinit token bucket (traffic classes)
+ *   1: Halt (traffic classes)
+ *   2: Flush (traffic classes)
+ *   3: Unhalt (no change to token bucket)
+ *   4: Flush to traffic class threshold (to byte and packet limit)
+ */
 struct ipahal_imm_cmd_shaping_control {
 	u64 traffic_class_bitmap[2];
 	u16 shaped_prod_token_bucket_reinit_bitmap;

drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal_fltrt.c
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
  * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.​
  */
 
 #include "ipa.h"
@@ -1273,16 +1274,10 @@ static u8 flt_action_to_hw_value(enum ipa_flt_action a)
 	case IPA_PASS_TO_EXCEPTION:
 		ret = 0x3;
 		break;
-	case IPA_PASS_TO_OUT_IPV4_CT_NAT:
-		ret = 0x4;
-		break;
-	case IPA_PASS_TO_IN_IPV4_CT_NAT:
-		ret = 0x5;
-		break;
 	default:
 		IPAHAL_ERR_RL("Invalid rule Action %d\n", a);
 		WARN_ON_RATELIMIT_IPA(1);
-		ret = 0x0;
+		ret = 0xff;
 	}
 
 	return ret;

drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal_fltrt.h
@@ -2,6 +2,7 @@
 /*
  * Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
  * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.​
  */
 
 #ifndef _IPAHAL_FLTRT_H_
@@ -63,6 +64,8 @@ enum ipahal_rt_rule_hdr_type {
  * @id: Rule ID
  * @cnt_idx: Stats counter index
  * @rule: Rule info
+ * @rule_type: Rule Type: ETH, IP etc
+ * @hpc_fetch_len: HPC fetch length field (for inline header insertion HPC)
  */
 struct ipahal_rt_rule_gen_params {
 	enum ipa_ip_type ipt;
@@ -75,7 +78,7 @@ struct ipahal_rt_rule_gen_params {
 	u8 cnt_idx;
 	const struct ipa_rt_rule_i *rule;
 	enum ipa_fltrt_rule_type rule_type;
-	u8 hpc_fetch_len;;
+	u8 hpc_fetch_len;
 };
 
 /*
@@ -123,6 +126,7 @@ struct ipahal_rt_rule_entry {
  * @id: Rule ID
  * @cnt_idx: Stats counter index
  * @rule: Rule info
+ * @rule_type: Rule Type: ETH, IP etc
  */
 struct ipahal_flt_rule_gen_params {
 	enum ipa_ip_type ipt;

drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal_fltrt_i.h
@@ -231,6 +231,31 @@ struct ipa5_5_rt_rule_hw_hdr {
 	} u;
 };
 
+/**
+ * struct ipa7_0_rt_rule_hw_hdr - HW header of IPA routing rule
+ * @en_rule: enable rule - Equation bit fields
+ * @dest_pipe_idx: destination pipe index
+ * @ingress_pol_dis: Ingress policing disable
+ * @close_aggr_irq_mod: close aggregation/coalescing and close GSI
+ *  interrupt moderation
+ * @ttl_update: TTL update
+ * @esp_after_udp: ESP in UDP for caching more than 5 tuple
+ * @rule_type: Rule type: IP, ETH, NonIP etc.
+ * @retain_hdr: added to add back to the packet the header removed
+ *  as part of header removal. This will be done as part of
+ *  header insertion block.
+ * @priority: Rule priority. Added to distinguish rules order
+ *  at the integrated table consisting from hashable and
+ *  non-hashable parts
+ * @shaping_traffic_class: Shaping Traffic Class
+ * @stats_cnt_idx_lsb: stats cnt index
+ * @rule_id: rule ID that will be returned in the packet status
+ * @system: Is referenced header is lcl or sys memory
+ * @hpc: whether hdr_offset points to header table or to
+ *	header processing context table
+ * @hpc_fetch_len: HPC fetch length (for inline header insertion HPC)
+ * @hdr_offset: header offset
+ */
 struct __attribute__((packed)) ipa7_0_rt_rule_hw_hdr {
 	u32 en_rule;
 
@@ -490,6 +515,30 @@ struct ipa5_5_flt_rule_hw_hdr {
 	} u;
 };
 
+/**
+ * struct ipa7_0_flt_rule_hw_hdr - HW header of IPA filter rule
+ * @en_rule: enable rule
+ * @rt_tbl_idx: index in routing table
+ * @reserved: Reserved
+ * @close_aggr_irq_mod: close aggregation/coalescing and close GSI
+ *  interrupt moderation
+ * @ttl_update: TTL update
+ * @esp_after_udp: ESP in UDP for caching more than 5 tuple
+ * @rule_type: Rule type: IP, ETH, NonIP etc.
+ * @retain_hdr: added to add back to the packet the header removed
+ *  as part of header removal. This will be done as part of
+ *  header insertion block.
+ * @priority: Rule priority. Added to distinguish rules order
+ *  at the integrated table consisting from hashable and
+ *  non-hashable parts
+ * @policing_traffic_class: Policing Traffic Class
+ * @stats_cnt_idx: stats cnt index
+ * @rule_id: rule ID that will be returned in the packet status
+ * @action: post filtering action
+ * @pdn_idx: in case of go to src nat action possible to input the pdn index to
+ *  the NAT block
+ * @set_metadata: enable metadata replacement in the NAT block
+ */
 struct __attribute__((packed)) ipa7_0_flt_rule_hw_hdr {
 	u64 en_rule                : 32;
 	u64 rt_tbl_idx             : 8;

drivers/platform/msm/ipa/ipa_v3/ipahal/ipahal_i.h
@@ -658,6 +658,28 @@ struct ipa_imm_cmd_hw_ip_packet_init_ex_v6_0 {
 	u64 rsvd6 : 7;
 } __packed;
 
+/*
+ * struct ipa_imm_cmd_hw_ip_packet_init_ex_v7_0 - IP_PACKET_INIT_EX command payload
+ *  in H/W format for IPA v7.0.
+ * @sw_reserved: Reserved for software use
+ * @conn_track_nat_stats_ip_type: IP type for connection tracking/NAT statistics
+ * @conn_track_nat_stats_direction: Direction for connection tracking/NAT statistics
+ * @traffic_mode: Traffic mode configuration
+ * @frag_disable: 1 - disabled. overrides IPA_ENDP_CONFIG_n:FRAG_OFFLOAD_EN
+ * @filter_disable: 1 - disabled, 0 enabled
+ * @nat_disable: 1 - disabled, 0 enabled
+ * @route_disable: 1 - disabled, 0 enabled
+ * @hdr_removal_insertion_disable: 1 - disabled, 0 enabled
+ * @cs_disable: 1 - disabled, 0 enabled
+ * @quota_tethering_stats_disable: 1 - disabled, 0 enabled
+ * @dpl_disable: 1 - disabled, 0 enabled
+ * @leading_header_size: Size of leading header in bytes
+ * @conn_track_nat_stats_counter_idx: Counter index for connection tracking/NAT statistics
+ * @rt_hdr_offset: Route header offset
+ * Fields @rt_pipe_dest_idx - @rt_hpc_fetch_len are routing rule parameters
+ * Fields @flt_rt_tbl_idx - @flt_set_metadata are filter rule parameters
+ * @sw_classification_cookie: Software classification cookie value
+ */
 struct __attribute__((packed)) ipa_imm_cmd_hw_ip_packet_init_ex_v7_0 {
 	u64 sw_reserved                      :12;
 	u64 conn_track_nat_stats_ip_type     :1;
@@ -1462,6 +1484,53 @@ struct ipa_frag_pkt_status_hw_v5_5 {
 	u64 hdr_local:1;
 } __packed;
 
+/*
+ * struct ipa_frag_pkt_status_hw_v7_0 -
+ * IPA v7.0 status packet payload in H/W format.
+ *  This structure describes the frag status packet H/W structure for the
+ *   following statuses: IPA_NEW_FRAG_RULE.
+ * @status_opcode: The Type of the status (Opcode).
+ * @frag_rule_idx: Frag rule index value.
+ * @reserved_1: reserved
+ * @exception: (not bitmask) - the first exception that took place.
+ * @tbl_idx: Table index valid or not.
+ * @endp_src_idx: Source end point index.
+ *  In case of exception, src endp and pkt len are always valid.
+ * @seq_num: Packet sequence number.
+ * @src_ip_addr: Source packet IP address.
+ * @dest_ip_addr: Destination packet IP address.
+ * @endp_dest_idx: Destination end point index.
+ * @protocol: Protocol number.
+ * @ip_id: IP packet IP ID number.
+ * @translated_ip_addr: IP address.
+ * @hdr_offset: Offset of used header in the header table
+ * @ip_cksum_diff: IP packet checksum difference.
+ * @metadata: meta data value used by packet
+ * @flt_stats_counter_idx: Filter statistics counter index.
+ * @ret: l2 header retained flag, indicates whether l2 header is retained
+ * or not.
+ * @low_latency_or_close_aggr_mod: Low latency or close aggregation mode flag.
+ * @ttl_dec: ttl update indication.
+ * @packet_type: Type of packet.
+ * @rt_stats_counter_idx: Route statistics counter index.
+ * @nat_or_ct: NAT or Connection Tracking flag.
+ * @hpc: Header Processing Context flag.
+ * @pd: Router disabled ingress policer.
+ * @hdr_local: Header table location flag: In header insertion, was the header
+ *  taken from the table resides in local memory? (If no, then system mem)
+ * @ergress_traffic_class: Egress traffic class index.
+ * @ingress_traffic_class: Ingress traffic class index.
+ * @reserved1: reserved
+ * @nat_type: Defines the type of the NAT operation:
+ *	00: No NAT
+ *	01: Source NAT
+ *	10: Destination NAT
+ *	11: Reserved
+ * @hpc_fetch_len: HPC fetch length.
+ * @ct_stats_counter_idx: Connection tracking statistics counter index.
+ * @reserved2: reserved
+ * @sw_classification_cookie: Software classification cookie.
+ */
 struct ipa_frag_pkt_status_hw_v7_0 {
 	u64 status_opcode:8;
 	u64 frag_rule_idx:4;
@@ -1604,6 +1673,76 @@ struct ipa_gen_pkt_status_hw_v6_0 {
 	u64 ucp:1;
 } __packed;
 
+/*
+ * struct ipa_gen_pkt_status_hw_v7_0 - IPA v7.0 status packet payload in H/W format.
+ *  This structure describes the status packet H/W structure for the
+ *   following statuses: IPA_STATUS_PACKET, IPA_STATUS_DROPPED_PACKET,
+ *   IPA_STATUS_SUSPENDED_PACKET.
+ *  Other statuses types has different status packet structure.
+ * @status_opcode: The Type of the status (Opcode).
+ * @exception: (not bitmask) - the first exception that took place.
+ *  In case of exception, src endp and pkt len are always valid.
+ * @status_mask: Bit mask specifying on which H/W blocks the pkt was processed.
+ * @pkt_len: Pkt pyld len including hdr, include retained hdr if used. Does
+ *  not include padding or checksum trailer len.
+ * @endp_src_idx: Source end point index.
+ * @pure_ack: Indicates pure ack TCP packet.
+ * @syn: Indicates TCP syn packet.
+ * @fin_rst: Indicates TCP fin/rst packet.
+ * @rt_local: Route table location flag: Does matching rt rule belongs to
+ *  rt tbl that resides in lcl memory? (if not, then system mem)
+ * @rt_cache_hit: Route cache hit flag: Does matching rt rule was in cache?
+ *  Not valid in case of exception
+ * @protocol_encoding: Defines the packet protocol:
+ * 	0 – None (protocol encoding is not set)
+ * 	1 – TCP
+ * 	2 – UDP
+ * 	3 – ICMP/ IPv6-ICMP (note that Status Mask already has an IPv4/IPv6 bit)
+ * @metadata: meta data value used by packet
+ * @flt_local: Filter table location flag: Does matching flt rule belongs to
+ *  flt tbl that resides in lcl memory? (if not, then system mem)
+ * @flt_cache_hit: Filter cache hit flag: Does matching flt rule was in cache?
+ * @ucp: UC Processing flag.
+ * @flt_ret_hdr: Retain header in filter rule flag: Does matching flt rule
+ *  specifies to retain header?
+ * @flt_rule_id: The ID of the matching filter rule. This info can be combined
+ *  with endp_src_idx to locate the exact rule. ID=0x3FF reserved to specify
+ *  flt miss. In case of miss, all flt info to be ignored
+ * @rt_tbl_idx: Index of rt tbl that contains the rule on which was a match
+ * @rt_rule_id: The ID of the matching rt rule. This info can be combined
+ *  with rt_tbl_idx to locate the exact rule. ID=0x3FF reserved to specify
+ *  rt miss. In case of miss, all rt info to be ignored
+ * @nat_entry_idx: Index of the NAT entry used of NAT processing
+ * @tag_info: S/W defined value provided via immediate command
+ * @seq_num: Per source endp unique packet sequence number
+ * @time_of_day_ctr: running counter from IPA clock
+ * @hdr_offset: Offset of used header in the header table
+ * @hpc: Header Processing Context flag
+ * @hdr_in_sys: Header in system memory flag
+ * @ttl_dec: ttl update flag, indicates whether ttl is updated.
+ * @frag_hit: Frag hit flag: Was their frag rule hit in H/W frag table?
+ * @packet_type: Type of packet
+ * @endp_dest_idx: Destination end point index.
+ * @ergress_traffic_class: Egress traffic class
+ * @ingress_traffic_class: Ingress traffic class
+ * @nat_hit: NAT hit flag: Was their NAT hit?
+ * @nat_type: Defines the type of the NAT operation:
+ *	00: No NAT
+ *	01: Source NAT
+ *	10: Destination NAT
+ *	11: Reserved
+ * @nat_cache_hit: NAT cache hit flag
+ * @nat_or_ct: NAT or Connection Tracking flag
+ * @nat_exc_suppress: nat exception supress flag, indicates whether
+ * nat exception is suppressed.
+ * @num_vlan_tags: Number of VLAN tags
+ * @flt_table_idx: Filter table index
+ * @frag_rule: Frag rule index in H/W frag table in case of frag hit
+ * @metadata_origin: Origin of metadata
+ * @reserved1: reserved
+ * @reserved2: reserved
+ * @reserved3: reserved
+ */
 struct ipa_gen_pkt_status_hw_v7_0 {
 	u64 status_opcode:8;
 	u64 exception:8;